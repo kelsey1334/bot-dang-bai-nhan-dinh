ef793d441f7c092f4f1ce4e38a6b044e86d9a758
kelsey1334 <kelsey@procons.one>
2025-07-23 22:52:06 +0700

    Update wp_poster.py

--- wp_poster.py
+++ wp_poster.py
@@ -1,36 +1,44 @@
-import os
-from wordpress_xmlrpc import Client, WordPressPost
-from wordpress_xmlrpc.methods.posts import NewPost
-from wordpress_xmlrpc.methods.media import UploadFile
-import mimetypes
+import requests
+import base64
 
-def get_wp_client(wp_url, wp_user, wp_pass):
-    return Client(wp_url, wp_user, wp_pass)
+def wp_basic_auth(user, app_password):
+    token = base64.b64encode(f"{user}:{app_password}".encode()).decode()
+    return {"Authorization": f"Basic {token}"}
 
-def upload_image_to_wp(client, image_path, title, alt, caption):
+def upload_image_to_wp(wp_url, user, app_password, image_path, alt, caption, title):
+    api_url = wp_url.rstrip('/') + '/wp-json/wp/v2/media'
+    headers = wp_basic_auth(user, app_password)
+    headers['Content-Disposition'] = f'attachment; filename="{image_path.split("/")[-1]}"'
+    headers['Content-Type'] = 'image/jpeg'
     with open(image_path, "rb") as img_file:
-        data = {
-            'name': os.path.basename(image_path),
-            'type': mimetypes.guess_type(image_path)[0] or 'image/jpeg',
-            'bits': img_file.read(),
-        }
-    response = client.call(UploadFile(data))
+        img_data = img_file.read()
+    params = {
+        'alt_text': alt,
+        'caption': caption,
+        'title': title
+    }
+    resp = requests.post(api_url, headers=headers, data=img_data, params=params)
+    resp.raise_for_status()
+    data = resp.json()
     return {
-        "url": response.url,
-        "id": response.id,
+        "url": data.get("source_url"),
+        "id": data.get("id"),
         "title": title,
         "alt": alt,
         "caption": caption
     }
 
-def post_to_wordpress(wp_url, wp_user, wp_pass, title, html_content, cat_id, img_list):
-    client = get_wp_client(wp_url, wp_user, wp_pass)
-    post = WordPressPost()
-    post.title = title
-    post.content = html_content
+def post_to_wordpress(wp_url, user, app_password, title, content, cat_id, img_list):
+    api_url = wp_url.rstrip('/') + '/wp-json/wp/v2/posts'
+    headers = wp_basic_auth(user, app_password)
+    params = {
+        'title': title,
+        'content': content,
+        'status': 'publish',
+        'categories': [cat_id],
+    }
     if img_list and img_list[0].get("id"):
-        post.thumbnail = img_list[0]["id"]
-    post.terms_names = {'category': [str(cat_id)]}
-    post.post_status = 'publish'
-    post_id = client.call(NewPost(post))
-    return post_id
+        params['featured_media'] = img_list[0]["id"]
+    resp = requests.post(api_url, headers=headers, json=params)
+    resp.raise_for_status()
+    return resp.json().get("id")
