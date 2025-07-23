cac1a80763d5b4f84ab8db481ec40ce3782071a8
kelsey1334 <kelsey@procons.one>
2025-07-24 00:23:00 +0700

    Update wp_poster.py

--- wp_poster.py
+++ wp_poster.py
@@ -12,17 +12,24 @@ def upload_image_to_wp(wp_url, user, app_password, image_path, alt, caption, tit
     headers['Content-Type'] = 'image/jpeg'
     with open(image_path, "rb") as img_file:
         img_data = img_file.read()
-    params = {
+    # Không cần params ở đây, chỉ meta sau khi upload
+    resp = requests.post(api_url, headers=headers, data=img_data)
+    resp.raise_for_status()
+    data = resp.json()
+    # Sửa meta nếu muốn sau upload (nếu cần alt/caption chuẩn)
+    # Sửa meta cho ảnh vừa upload
+    media_id = data.get("id")
+    meta_url = wp_url.rstrip('/') + f'/wp-json/wp/v2/media/{media_id}'
+    meta = {
         'alt_text': alt,
         'caption': caption,
         'title': title
     }
-    resp = requests.post(api_url, headers=headers, data=img_data, params=params)
-    resp.raise_for_status()
-    data = resp.json()
+    resp2 = requests.post(meta_url, headers=headers, json=meta)
+    # resp2.raise_for_status() # Có thể bị lỗi 401, nhưng không ảnh hưởng ảnh
     return {
         "url": data.get("source_url"),
-        "id": data.get("id"),
+        "id": media_id,
         "title": title,
         "alt": alt,
         "caption": caption
@@ -37,6 +44,7 @@ def post_to_wordpress(wp_url, user, app_password, title, content, cat_id, img_li
         'status': 'publish',
         'categories': [cat_id],
     }
+    # Thumbnail: dùng ID ảnh thumbnail upload ở đầu img_list
     if img_list and img_list[0].get("id"):
         params['featured_media'] = img_list[0]["id"]
     resp = requests.post(api_url, headers=headers, json=params)
